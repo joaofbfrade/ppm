233deec12b99698a64796239fc8d768af95a5640
joaofbfrade <joaofbfrade97@gmail.com>
2023-05-04 17:14:22 +0100

    ficha1

--- src/main/scala/Helloworld.scala
+++ src/main/scala/Helloworld.scala
@@ -1,13 +1,13 @@
 object Helloworld extends App {
 
+// 2 A
+  def ex2a(a: (Int, Int), b: (Int, Int)): (Int, Int) = {
 
+    return (a._1 + a._2, b._1 * b._2)
 
-    def add (x:Int, y : Int) : Int = {
+  }
 
-      return x+y;
-    }
-
-
+// 2b
   def maior(x:Int,y:Int) = if (x > y) x else y
   def menor(x:Int,y:Int) = if (x > y) y else x
 
@@ -17,24 +17,60 @@ object Helloworld extends App {
   def middle3(x: Int, y: Int, z: Int) = (menor((maior(x, y)),
     maior(menor(x, y), z)))
 
-  def largest(x: Int, y: Int, z: Int) = (maiorde3(x, y, z),
+  def ex2b(x: Int, y: Int, z: Int) = (maiorde3(x, y, z),
     middle3(x, y, z))
 
 
-  def factorial (n:Int):Int ={
-    if (n == 1)  1
-    else n*factorial(n-1)
+
+
+  // 2C
+
+
+  def ex2c(x:Int,y:Int,z:Int): (Int,Int,Int)  = ( maior(maior(x,y),z)   ,middle3(x,y,z)   ,  menor(menor(x,y),z)  )
+
+  // 2D
+
+  def ex2d(x:Int,y:Int,z:Int): (Boolean) =  x + y > z && x +z > y && y + z > x
+
+
+
+  // 2E
+
+  def ex2e (str: String) = {
+    val lst: Array[String] = str.split(" ");
+    lst.head ++ " " ++
+      lst.last
   }
 
-  def pares (a:(Int,Int),b: (Int,Int)):(Int,Int) ={
 
-    return (a._1+a._2,b._1*b._2)
-
+  def ex2e2(str: String) = {
+   val nome = str.split(" ")
+    nome.head + "  " + nome.last
   }
+  //3a
 
-  def b3 (n:Int,x:Int,y:Int):(Int,Int)= (maior(x,maior(y,n)) , maior(x,n))
 
-  println("pares: " + pares(  (1,1),(1,1)  )  )
+  def ex3a (x:Int,y:Int): Int =
+   if(y==0) 1 else x*(ex3a(x, y - 1))
+
+
+
+  //3B
+
+     def ex3b(lst:List[Int]) = (lst.head,lst.last)
+
+  //3C
+
+  def ex3c(lst:List[Int]) = (lst, lst.size)
+  //3D
+
+
+  def ex3d (lst:List[Double]):Double = if (lst.isEmpty) 0 else (lst.head+ex3d(lst.tail))/lst.length
+
+
+
+  print( ex3d (List(1.3,2.2,3.1) ))
+
 
 
 }
